b0ed5918265a6c493123d456b58027dd5e2dc7c1
developerdiegorodrigues <developer.diego.rodrigues@gmail.com>
2024-06-12 10:52:33 -0300

    Initial testing in the environment

--- src/content.js
+++ src/content.js
@@ -1,1 +1,15 @@
-console.log(">troo-x-video-downloader started");
+console.log(">Troo-x-video-downloader: Started");
+console.log(">Troo-x-video-downloader: Listener registration");
+
+window.addEventListener('load', function () {
+
+    console.log(">Troo-x-video-downloader: Checking domain");
+    const currentDomain = window.location.hostname;
+
+    if (currentDomain != "www.tests.com") {
+        console.log(">Troo-x-video-downloader: Not the expected domain");
+        return;
+    }
+
+    console.log(">Troo-x-video-downloader: Expected domain");
+});
